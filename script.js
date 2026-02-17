e01807677f878b51f5ed17cd9297decf627ab042
riceBowl49-51 <cavhacate29@src.pshs.edu.ph>
2026-02-17 09:53:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+const words = [
+    "PLANT", "BRICK", "MOUSE", "TREND", "SHOUT",
+    "FLARE", "COAST", "GRIND", "PIANO", "WRECK",
+    "JUMPY", "HUMID", "SCARF", "TOKEN", "BLEND",
+    "SWIFT", "CHALK", "DRONE", "MAPLE", "FOUNT"
+];
+var randomWord = words[Math.floor(Math.random() * words.length)];
+var wordArray = randomWord.split("");
+const MAXSCORE = 5;
+var gameOver = false;
+
+var row = 1;
+var col = 1;
+
+const board = [
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""]
+];
+
+function onLetterClicked(letter) {
+    if (gameOver) return;
+    if (col > 5 || row > 6) return;
+
+    board[row - 1][col - 1] = letter;
+
+    document.getElementById("r" + row + "c" + col).innerText = letter;
+
+    col++;
+}
+
+function onDelClicked() {
+    if (gameOver) return;
+    if (col == 1) return;
+    col--;
+
+    board[row - 1][col - 1] = "";
+
+    document.getElementById("r" + row + "c" + col).innerText = "";
+}
+
+function onEnterClicked() {
+    if (col <= 5) {
+        document.getElementById("result").innerText = "Finish the word first!";
+        return;
+    }
+
+    document.getElementById("result").innerText = "";
+
+    let score = 0;
+
+    for (let index = 0; index < 5; index++) {
+        let guessedLetter = board[row - 1][index];
+
+        if (guessedLetter == wordArray[index]) {
+            document.getElementById("r" + row + "c" + (index + 1)).style.backgroundColor = "green";
+            score++;
+        }
+        else if (wordArray.includes(guessedLetter)) {
+            document.getElementById("r" + row + "c" + (index + 1)).style.backgroundColor = "yellow";
+        }
+        else {
+            document.getElementById("r" + row + "c" + (index + 1)).style.backgroundColor = "red";
+        }
+    }
+
+    if (score == MAXSCORE) {
+        document.getElementById("result").innerText = "WoW u win!";
+        document.getElementById("restart").style.visibility = "visible";
+        gameOver = true;
+        return;
+    }
+    else if (score != MAXSCORE && row == 6) {
+        document.getElementById("result").innerText = "HAHA- u loser";
+        gameOver = true;
+        return;
+    }
+
+    row++;
+    col = 1;
+}
+
+function resetButton() {
+    gameOver = false;
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    wordArray = randomWord.split("");
+
+    for (let r = 0; r < 6; r++) {
+        for (let c = 0; c < 5; c++) {
+            board[r][c] = "";
+            let cell = document.getElementById("r" + (r + 1) + "c" + (c + 1));
+            cell.innerText = "";
+            cell.style.backgroundColor = "";
+        }
+    }
+
+    row = 1;
+    col = 1;
+
+    document.getElementById("result").innerText = "";
+
+    document.getElementById("restart").style.visibility = "hidden";
+}
